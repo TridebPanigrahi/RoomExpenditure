0c41830af68f0f6e19e956fd30066fca6a6475e0
trideb-religh <panigrahitrideb1@gmail.com>
2023-12-26 23:51:47 +0530

    Updated code

--- web/src/App.js
+++ web/src/App.js
@@ -1,53 +1,15 @@
-import React, { useState } from "react";
-import "./App.css";
-import Camera from "./components/Camera";
+import React from 'react'
+// import './App.css'
+import Home from './screens/Home'
 
-function App() {
-  const [data, setData] = useState([]);
-  const handelChange = (e) => {
-    setData({
-      ...data,
-      [e.target.name]: e.target.value,
-    });
-  };
-  console.log(data);
-  const handelSubmit = async (e) => {
-    e.preventDefault();
-    const response = await fetch("http://localhost:8080/usersData", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" },
-    });
-    const res = response.json();
-    console.log(res);
-  };
+const App = () => {
   return (
-    // <>
-    //   <form onSubmit={handelSubmit}>
-    //     <input
-    //       type="text"
-    //       name="userName"
-    //       placeholder="Enter Name"
-    //       onChange={handelChange}
-    //     />
-    //     <input
-    //       type="password"
-    //       name="password"
-    //       placeholder="Enter Password"
-    //       onChange={handelChange}
-    //     />
-    //     <button type="submit">SUBMIT</button>
-    //   </form>
-    // </>
     <>
-      <header className="App-header">
-        <h1>Camera App</h1>
-      </header>
-      <main>
-        <Camera />
-      </main>
+    <div>
+      <Home/>
+    </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
